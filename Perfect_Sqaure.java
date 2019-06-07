c08b5c51dcd782fed53043030bbca04fc4aa6384
Arjun38 <arjun3847@gmail.com>
2019-06-07 12:28:05 -0400

    initial commits from arjun

---
+++ Perfect_Sqaure.java
@@ -0,0 +1,8 @@
+import java.io.*;
+
+public interface Perfect_Sqaure {
+
+	boolean method (int x);
+
+
+}
---
+++ runnerB.java
@@ -0,0 +1,14 @@
+public class runnerB {
+
+	public runnerB(){
+		Perfect_Sqaure q =(x)->{
+		return true;};
+		}
+
+
+		public static void main(String []args){
+
+			runnerB app = new runnerB();
+			}
+
+}
