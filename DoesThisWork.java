90f260f8b104ea2b64c1068db24b89eaeb5b9277
kushanand7 <kushanand7@gmail.com>
2019-06-07 12:06:22 -0400

    change it up

--- DoesThisWork.java
+++ DoesThisWork.java
@@ -1,10 +1,13 @@
 public class DoesThisWork
 {
 	public DoesThisWork(){
-		AndWeQuote innerface = (x, y) -> {
+		MyInterface q = (x, y) -> {
 			return x/y;
 		};
-		System.out.printf("" + innerface.quotient(5f, 2f));
+		float number = q.quotient(10f,3f);
+		int temp = (int)(number*1000);
+		number=temp/1000f;
+		System.out.printf("" + number);
 	}
 	public static void main(String kushandarjun[]){
 		DoesThisWork app = new DoesThisWork();
