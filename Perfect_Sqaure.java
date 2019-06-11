7151689de305836709a05b8232a2af500d9e740b
kushanand7 <kushanand7@gmail.com>
2019-06-11 12:02:17 -0400

    Revert "im done with my part"
    This reverts commit ddb328ef57d225127d93dcd3d327ce586f2a5b3e.

--- Perfect_Sqaure.java
+++ Perfect_Sqaure.java
@@ -3,8 +3,6 @@ import java.io.*;
 public interface Perfect_Sqaure {
 
 	boolean method (int x);
-	//yo this is kush i figured out how to get his stuff and if you're reading this then i figured out how to upload it back
-	//yeet
-	//father schiff
-	//the church of android
+
+
 }
--- runnerB.java
+++ runnerB.java
@@ -4,13 +4,11 @@ public class runnerB {
 		Perfect_Sqaure q =(x)->{
 		return true;};
 		}
-		//hi its kush i can mess with this now
+
 
 		public static void main(String []args){
 
 			runnerB app = new runnerB();
 			}
-		//joasdfpoha
-		//OIHASDOPFH
-		//i dont like school
+
 }
